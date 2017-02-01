d6ac493be385b0d5170a877d4d74eb6f98bdfe17
Alexander Polyakov <alx.polyakov@gmail.com>
2017-02-01 03:45:32 +0300

    remove

--- plugins/vmpayment/cryptocoin/tables/cryptocoin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class TableCryptoCoin extends JTable{
-    public function __construct(&$db)
-    {
-        parent::__construct('#__virtuemart_payment_plg_cryptocoin', 'id', $db);
-    }
-}
